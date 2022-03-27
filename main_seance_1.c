3ea1a16f41085fa1f68c3882bba14740fa0a903a
Dana Diab <81438801+DanaDiab@users.noreply.github.com>
2022-03-27 23:17:46 +0200

    Update main_seance_1.c

--- main_seance_1.c
+++ main_seance_1.c
@@ -28,20 +28,20 @@ int main(int argc, char *argv[])
 	assert(modpow_naive(4555555,80,3)==modpow_rec(4555555,80,3));
 	printf("%ld\n", random_prime_number(5,6,10));
 
-	long p=5, q=6, n=0, s=0,u=0;
-	generate_key_values(p, q, &n, &s, &u);
+	long p=5, q=6, n=0, s=0,u=0;		//Test de la fonction qui genère une clé publique et secrète avec un p et q donnés 
+	generate_key_values(p, q, &n, &s, &u);		
 	printf("p=%ld, q=%ld, *n=%ld, *s=%ld, *u=%ld\n", p,q,n,s,u);
 
 
 	FILE *fichier=fopen("sortie_vitesse_modpow.txt","w"); //Ouverture d'un fichier
 	for (int i=0;i<1000000;i++){ //boucle qui calcul le temps d'execution pour chaque fonction
-		temps_mpnaive_init=clock();
+		temps_mpnaive_init=clock();	//temps auquel la fonction modpow_naive s'execute
 		modpow_naive(3,i,5);
-		temps_mpnaive_fin=clock();
+		temps_mpnaive_fin=clock();	//temps auquel la fonction modpow_naive se termine
 		temps_mp_init=clock();
 		modpow(3,i,5);
 		temps_mp_fin=clock();
-		temps_mpnaive=((double)(temps_mpnaive_fin-temps_mpnaive_init))/CLOCKS_PER_SEC;
+		temps_mpnaive=((double)(temps_mpnaive_fin-temps_mpnaive_init))/CLOCKS_PER_SEC; //Calcul tu temps en nombre de secondes.
 		temps_mp= ((double)(temps_mp_fin-temps_mp_init))/CLOCKS_PER_SEC;
 		fprintf(fichier,"%d %lf %lf\n", i,temps_mpnaive,temps_mp); //Ecriture dans un fichier
 	}
