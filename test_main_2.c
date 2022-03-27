75aaf4a315a794dbd3f704679da1e6adf40a9ab4
Dana Diab <81438801+DanaDiab@users.noreply.github.com>
2022-03-27 23:49:13 +0200

    Update test_main_2.c

--- test_main_2.c
+++ test_main_2.c
@@ -10,62 +10,67 @@ int main(void){
   srand(time(NULL));
 
   //Testing Init Keys
-  Key* pKey=malloc(sizeof(Key));
-  Key* sKey= malloc(sizeof(Key));
-  init_pair_keys(pKey,sKey,3, 7);
-  printf("pKey: %lx, %lx \n",pKey->k,pKey->n);
-  printf("sKey: %lx, %lx \n",sKey->k,sKey->n);
+	Key* pKey=malloc(sizeof(Key));
+  	Key* sKey= malloc(sizeof(Key));
+  	init_pair_keys(pKey,sKey,3, 7);
+  	printf("pKey: %lx, %lx \n",pKey->k,pKey->n);
+  	printf("sKey: %lx, %lx \n",sKey->k,sKey->n);
 
   //Testing Key Serialization
-  char * chaine=key_to_str(pKey);
-  printf("key_to_str: %s \n",chaine);
-  Key* k=str_to_key(chaine);
-  printf("str_to_key: %lx, %lx \n",k->k,k->n);
+	char * chaine=key_to_str(pKey);
+  	printf("key_to_str: %s \n",chaine);
+  	Key* k=str_to_key(chaine);
+	printf("str_to_key: %lx, %lx \n",k->k,k->n);
 	free(chaine);
 
-  //Testing signature
-  //Candidate keys :
-  Key* pKeyC=malloc(sizeof(Key));
-  Key* sKeyC=malloc(sizeof(Key));
-  init_pair_keys(pKeyC, sKeyC,3,7);
+  	//Testing signature
+  	//Candidate keys :
+  	Key* pKeyC=malloc(sizeof(Key));
+  	Key* sKeyC=malloc(sizeof(Key));
+  	init_pair_keys(pKeyC, sKeyC,3,7);
 
-  //Declaration
-  char *mess= key_to_str(pKeyC);
+  	//Declaration
+  	char *mess= key_to_str(pKeyC);
 	char* pkey_str=key_to_str(pKey);
-  printf("%s %s\n",pkey_str, mess);
+  	printf("%s %s\n",pkey_str, mess);
 	free(pkey_str);
-  Signature * sgn=sign(mess,sKey);
-  printf("signature: ");
-  print_long_vector(sgn->content,sgn->size);
-  chaine=signature_to_str(sgn);
-  printf("signature_to_str: %s \n",chaine);
+  	Signature * sgn=sign(mess,sKey);
+	printf("signature: ");
+	print_long_vector(sgn->content,sgn->size);
+	chaine=signature_to_str(sgn);
+	printf("signature_to_str: %s \n",chaine);
 	free(sgn->content);
 	free(sgn);
-  sgn=str_to_signature(chaine);
-  printf("str_to_signature: ");
-  print_long_vector(sgn->content,sgn->size);
+	sgn=str_to_signature(chaine);
+	printf("str_to_signature: ");
+	print_long_vector(sgn->content,sgn->size);
 
-  //Testing protected:
-  Protected* pr=init_protected(pKey,mess,sgn);
+  	//Testing protected:
+  	Protected* pr=init_protected(pKey,mess,sgn);
 
-  //Verification
-  if(verify(pr)){
-    printf("Signature valide\n");}
-  else{
-    printf("Signature non valide\n");
-  }
+	//Verification de la signature
+	if(verify(pr)){
+		printf("Signature valide\n");}
+  	else{
+   		printf("Signature non valide\n");
+  	}
+	
 	free(chaine);
-
-  chaine=protected_to_str(pr);
-  printf("protected_to_str: %s\n",chaine );
-
+	
+	//Serialisation de protected
+  	chaine=protected_to_str(pr);
+  	printf("protected_to_str: %s\n",chaine );
 	free(pr->mess);
 	free(pr);
-  pr=str_to_protected(chaine);
+	
+	//Deserialisation de protected
+ 	pr=str_to_protected(chaine);
 	char* key_str=key_to_str(pr->pkey);
 	char *sign_str=signature_to_str(pr->sign);
-  printf("str_to_protected: %s %s %s\n",key_str,pr->mess,sign_str);
+  	printf("str_to_protected: %s %s %s\n",key_str,pr->mess,sign_str);
 
+	
+	//Liberation de mémoire : 
 	free(key_str);
 	free(sign_str);
 	free(chaine);
@@ -82,5 +87,5 @@ int main(void){
 	free(pr->sign->content);
 	free(pr->sign);
 	free(pr);
-  return 0;
+  	return 0;
 }
