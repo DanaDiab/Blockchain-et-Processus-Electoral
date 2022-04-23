2e67a6f0d3dc0960cd026c8f22d88d4e88721d98
Dana <81438801+DanaDiab@users.noreply.github.com>
2022-04-23 23:20:15 +0200

    Done

--- main_seance_7.c
+++ main_seance_7.c
@@ -20,16 +20,21 @@ int main()
 	char *fichier_voters="keys.txt";
 	char *fichier_candidates="candidates.txt";
 	char *fichier_decl="declarations.txt";
+	
+	//Creations des listes
 	CellKey* voters=read_public_keys(fichier_voters);
 	CellKey* candidates=read_public_keys(fichier_candidates);	
 	CellProtected* decl=read_protected(fichier_decl);
 
+	//Soumission du vote dans le fichier "Pending_votes.txt"
 	submit_vote(decl->data);
+	
+	//Creation du Block qui sera dans la racine comme l'arbre passé en paramètre est NULL.
 	CellTree* tree=NULL;
-	createBlock(tree,voters->data, 2);
-	add_block(2,"Block1.txt");
+	createBlock(tree,voters->data, 2);	//valeur hachée commencant par 2 zéros
+	add_block(2,"Block1.txt");		//Ecriture du Block dans le fichier /Blockchain/Block1.txt
 	
-	
+	//Liberation de memoire
 	delete_tree(tree);
 	delete_list_keys(voters);
 	delete_list_keys(candidates);
