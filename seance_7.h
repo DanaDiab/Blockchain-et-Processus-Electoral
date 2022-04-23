2cb7699c463fefa3894c4bb4c47b3663986d8645
Dana <81438801+DanaDiab@users.noreply.github.com>
2022-04-23 20:24:40 +0200

    Done

--- seance_7.h
+++ seance_7.h
@@ -6,4 +6,5 @@ void createBlock(CellTree* tree,Key* author, int d);
 void add_block(int d,char * name);
 CellTree* read_tree();
 Key * compute_winner_BT(CellTree* tree, CellKey * candidates, CellKey* voters,int sizeC, int sizeV);
+
 #endif
