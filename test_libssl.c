897b4e43fde510f0199582f67ab73151fe9d1d26
Dana <81438801+DanaDiab@users.noreply.github.com>
2022-04-23 20:34:18 +0200

    Done

--- test_libssl.c
+++ test_libssl.c
@@ -4,9 +4,10 @@
 #include <string.h>
 
 int main(){
+	
 	const char * s= "Rosetta code";
-	unsigned char *d = SHA256((unsigned char *)s,strlen(s),0);
-	for(int i=0; i<SHA256_DIGEST_LENGTH ;i++){
+	unsigned char *d = SHA256((unsigned char *)s,strlen(s),0);		//Hachage de la chaine "Rosetta code";
+	for(int i=0; i<SHA256_DIGEST_LENGTH ;i++){				//Affichage de la valeur hachée en hexadecimal
 		printf("%02x",d[i]);
 	}
 	putchar('\n');
