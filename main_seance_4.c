f58edc8b374e163cbc0083a657678771d5f520b9
Dana <81438801+DanaDiab@users.noreply.github.com>
2022-04-23 22:14:51 +0200

    Done

--- main_seance_4.c
+++ main_seance_4.c
@@ -17,10 +17,12 @@ int main()
 	char *fichier_candidates="candidates.txt";
 	char *fichier_decl="declarations.txt";
 
+	//Declaration et Initialisation des 3 listes à partir des 3 fichiers
 	CellKey* voters=read_public_keys(fichier_voters);
 	CellKey* candidates=read_public_keys(fichier_candidates);	
 	CellProtected* decl=read_protected(fichier_decl);
 
+	//Affichage
 	printf("LISTE VOTANTS :\n");
 	print_list_keys(voters);
 	printf("***************************\n");
@@ -32,21 +34,27 @@ int main()
 	printf("LISTE DECLARATION AVANT CONTROLE:\n");	
 	print_list_protected(decl);		
 	printf("***************************\n");
-		
+	
+	//Suppression des votes non valides
 	delete_non_valide(&decl);
 
+	//Affichage
 	printf("LISTE DECLARATION APRES CONTROLE :\n");	
 	print_list_protected(decl);
 	printf("**************************\n");				
 	
+	//Recherche du gagnant, sa serialisation et son affichage
 	Key* winner=compute_winner(decl, candidates, voters,10, 30);
 	char * gagnant=key_to_str(winner);
 	printf("Le gagnant des éléctions est : %s \n", gagnant);
 	
+	
+	//Liberation de memoire
 	delete_list_keys(voters);
 	delete_list_keys(candidates);
 	delete_list_protected(decl);
 	free(gagnant);
 	free(winner);
+	
 	return 0;
 }
