4166928b6ac43422a4844db820a96eda9831f400
Dana <81438801+DanaDiab@users.noreply.github.com>
2022-04-23 22:00:24 +0200

    Done

--- main_seance_3.c
+++ main_seance_3.c
@@ -10,19 +10,26 @@
 int main(){
 	srand(time(NULL));
 	
-	char *fichier="keys.txt";	//nom du fichier dans lequel sont écrit les clés publiques et secrètes des candidats
-	CellKey* LCK=read_public_keys(fichier);		//Creation de la liste chainée de CellKey qui contient les valeurs dans 'fichier'
+	char *fichier="keys.txt";			//nom du fichier contenant les clés publiques et secrètes des votants
+	CellKey* LCK=read_public_keys(fichier);		//Creation de la liste chainée de CellKey qui contient les clés publiques dans 'fichier'
+	
 	print_list_keys(LCK);				//Affichage des datas (Keys) de la liste LCP
+	
 	delete_list_keys(LCK);				//Libération de la mémoire allouée pour la list LCP
 
 
-	char *fichier1="declarations.txt";
-	CellProtected* LCP=read_protected(fichier1);		//Creation de la liste chainée de CellKey qui contient les valeurs dans 'declarations.txt'
+	
+	char *fichier1="declarations.txt";		//nom du fichier contenant les votes
+	CellProtected* LCP=read_protected(fichier1);	//Creation de la liste chainée de CellProtected qui contient les votes dans 'declarations.txt'
+	
 	printf("\n\nListe avant controle : \n\n");
 	print_list_protected(LCP);			//Affichage de la liste
+	
 	delete_non_valide(&LCP);			//Suppression des éléments de la liste LCP dont la signature n'est pas valide
+	
 	printf("\n\nListe après controle : \n\n");
-	print_list_protected(LCP);			//Affichage de la liste pour bien s'assurer de la suppression (ce qui a été approuvé)
+	print_list_protected(LCP);			//Affichage de la liste pour bien s'assurer de la suppression (ce qui a bien été remarqué)
+	
 	delete_list_protected(LCP);			//Suppression du reste de la liste
 	return 0;
 }
