de64ee1822c95b17696ed8ef06ea905935930bea
Dana <81438801+DanaDiab@users.noreply.github.com>
2022-04-23 20:34:42 +0200

    Done

--- test_libssl.c
+++ test_libssl.c
@@ -6,7 +6,7 @@
 int main(){
 	
 	const char * s= "Rosetta code";
-	unsigned char *d = SHA256((unsigned char *)s,strlen(s),0);		//Hachage de la chaine "Rosetta code";
+	unsigned char *d = SHA256((unsigned char *)s,strlen(s),0);		//Hachage de la chaine "Rosetta code" à l'aide de la fonction SHA256;
 	for(int i=0; i<SHA256_DIGEST_LENGTH ;i++){				//Affichage de la valeur hachée en hexadecimal
 		printf("%02x",d[i]);
 	}
