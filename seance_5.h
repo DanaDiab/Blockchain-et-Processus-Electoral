6febedea9cd32f681cd98bef85c949a690555512
Dana <81438801+DanaDiab@users.noreply.github.com>
2022-04-24 18:12:54 +0200

    Done

--- seance_5.h
+++ seance_5.h
@@ -9,12 +9,17 @@ typedef struct block{
 	int nonce;
 }Block;
 
-Block * create_block (Key * author, CellProtected* votes,  unsigned char * hash, unsigned char * prev_hash,int nonce); //Fonction non demandé: qui crée un Block avec les valeurs passé en paramtère, allocation dynamique du Block, et des 2 valeurs hachées
+//Fonction non demandé: qui crée un Block avec les valeurs passé en paramtère, allocation dynamique du Block, et des 2 valeurs hachées
+Block * create_block (Key * author, CellProtected* votes,  unsigned char * hash, unsigned char * prev_hash,int nonce);
+
 void block_to_file(Block* b, char* fichier);
 Block* file_to_block(char *fichier);
 char* block_to_str(Block *block);
 unsigned char * hashage(const char *s);
 void compute_proof_of_work(Block *b , int d);
 int verify_block(Block *b, int d);
-void delete_block(Block *b); //supprime un block mais ne libère pas le champ 'author'. Libère la liste cell protected mais pas leur élément protected.
+
+ //Fonction qui supprime un block mais ne libère pas le champ 'author'. Libère la liste cell protected mais pas leur élément protected.
+void delete_block(Block *b);
+
 #endif
